5e5288745980d49e4b9ddcae7b44e9d2c1c95369
Tejaswee Gattu <tejaswee.gattu@motivitylabs.com>
2023-03-03 10:38:59 +0530

    Ado_crud

--- UserInterface_Layer/Controllers/AdoCollegeController.cs
+++ UserInterface_Layer/Controllers/AdoCollegeController.cs
@@ -10,8 +10,140 @@ namespace UserInterface_Layer.Controllers
     [ApiController]
     public class AdoCollegeController : ControllerBase
     {
+        //  string ConnectionInformation = "Data Source=MLI01139;Initial Catalog=Clg_Dept_Student_Db;User ID=sa;Password=***********;Integrated security=True;Trusted_Connection=True;TrustServerCertificate=True";
+
+        // string ConnectionInformation= ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        string ConnectionInformation = Configuration["ConnectionStrings:DefaultConnection"];
+
+        public SqlConnection MainConnection;
         public AdoCollegeController()
         {
+            MainConnection = new SqlConnection(ConnectionInformation);
+            MainConnection.Open();
+        }
+
+
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+
+            //DataTable dt = new DataTable();
+            //string MyCommand = "Select * from College";
+            //SqlCommand myCommand = new SqlCommand(MyCommand, MainConnection);
+            //SqlDataAdapter da = new SqlDataAdapter(myCommand);
+            //da.Fill(dt);
+
+            //List<College> entities = new List<College>(dt.Rows.Count);
+            //if (dt.Rows.Count > 0)
+            //{
+            //    foreach (DataRow record in dt.Rows)
+            //    {
+            //        College item = GetItem<College>(record);
+            //        entities.Add(item);
+            //    }
+
+            //}
+            //return Ok(entities);
+            SqlDataReader reader = null;
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Data Source=MLI01139;Initial Catalog=Clg_Dept_Student_Db;User ID=sa;Password=***********;Integrated security=True;Trusted_Connection=True;TrustServerCertificate=True";
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Select * from College";
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            reader = sqlCmd.ExecuteReader();
+            College college = null;
+            while (reader.Read())
+            {
+                college = new College();
+                college.Cid = Convert.ToInt32(reader.GetValue(0));
+                college.Cname = reader.GetValue(1).ToString();
+                college.Caddress= reader.GetValue(1).ToString(); ;
+            }
+            return college;
+            myConnection.Close();
+        }
+        //private static College GetItem<T>(DataRow dr)
+        //{
+        //    Type temp = typeof(College);
+        //    College obj = Activator.CreateInstance<College>();
+
+        //    foreach (DataColumn column in dr.Table.Columns)
+        //    {
+        //        foreach (PropertyInfo pro in temp.GetProperties())
+        //        {
+        //            if (pro.Name == column.ColumnName)
+        //                pro.SetValue(obj, dr[column.ColumnName], null);
+        //            else
+        //                continue;
+        //        }
+        //    }
+        //    return obj;
+        //}
+
+        [HttpGet("GetById{Id}")]
+        public IActionResult GetCollegeById(int Id)
+        {
+            DataTable dt = new DataTable();
+            string MyCommand = "Select * from College where Cid=" + Id;
+            SqlCommand myCommand = new SqlCommand(MyCommand, MainConnection);
+            SqlDataAdapter da = new SqlDataAdapter(myCommand);
+            da.Fill(dt);
+
+            List<College> entities = new List<College>(dt.Rows.Count);
+            if (dt.Rows.Count > 0)
+            {
+                College item = GetItem<College>(dt.Rows[0]);
+                entities.Add(item);
+            }
+            else return null;
+
+            return Ok(entities);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] College college)
+        {
+            string InsertCommand = "INSERT INTO College (Cid,Cname,Caddress,Czipcode,ActiveFlag,CreatedDate,CreatedBy,ModifiedBy,ModifiedDate) VALUES(@Cid,@Cname,@Caddress,@Czipcode,@ActiveFlag,@CreatedDate,@Createdby,@ModifiedBy,@ModifiedDate)";
+            try
+            {
+                SqlCommand insertCommand = new SqlCommand(InsertCommand, MainConnection);
+                insertCommand.Parameters.AddWithValue("@Cid", college.Cid);
+                insertCommand.Parameters.AddWithValue("@Cname", college.Cname);
+                insertCommand.Parameters.AddWithValue("@Caddress", college.Caddress);
+                insertCommand.Parameters.AddWithValue("@Czipcode", college.Czipcode);
+                insertCommand.Parameters.AddWithValue("@ActiveFlag", college.ActiveFlag);
+                insertCommand.Parameters.AddWithValue("@CreatedBy", college.CreatedBy);
+                insertCommand.Parameters.AddWithValue("@CreatedDate", college.CreatedDate);
+                insertCommand.Parameters.AddWithValue("@ModifiedBy", college.ModifiedBy);
+                insertCommand.Parameters.AddWithValue("@ModifiedDate", college.ModifiedDate);
+
+                var result = insertCommand.ExecuteNonQuery();
+                if (result > 0)
+                    return Ok();
+                else
+                    return BadRequest();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] College college)
+        {
+            string InsertCommand = "UPDATE Users SET Cname=@Cname,Caddress=@Caddress,Czipcode=@Czipcode WHERE Cid=" + college.Cid;
+            try
+            {
+                SqlCommand insertCommand = new SqlCommand(InsertCommand, MainConnection);
+                insertCommand.Parameters.AddWithValue("@Cname", college.Cname);
+                insertCommand.Parameters.AddWithValue("@Caddress", college.Caddress);
+                insertCommand.Parameters.AddWithValue("@Czipcode", college.Czipcode);
 
         }
     }
