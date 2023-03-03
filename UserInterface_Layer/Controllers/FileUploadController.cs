547335c76dc9da530c6c8eb3937e0044c1682d86
Tejaswee Gattu <tejaswee.gattu@motivitylabs.com>
2023-03-03 10:30:41 +0530

    ADO_Crud

---
+++ UserInterface_Layer/Controllers/FileUploadController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserInterface_Layer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+    }
+}
